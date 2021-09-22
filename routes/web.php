ad9cad210c92e03e6dd008f343a4ab50de9bc5ce
NumahataSaori <saorin.kimura@gmail.com>
2021-09-22 15:32:54 +0000

    2021.09.22_php/laravel 09

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route:: group(['prefix' => 'admin'], function() {
+    
+    Route:: get('news/create', 'Admin\NewsController@add');
+    
+    Route:: get('profile/create', 'Admin\ProfileController@add');
+    Route:: get('profile/edit', 'Admin\ProfileController@edit');
+    
+});
+
+
+//Route:: get('XXX','AAAController@bbb');
