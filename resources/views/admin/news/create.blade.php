cc967a38c23e8d27a687c86d516c358618c6b4e0
NumahataSaori <saorin.kimura@gmail.com>
2021-09-23 05:59:35 +0000

    2021.09.23_php/laravel 10

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MyNews</title>
+    </head>
+    
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+    
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MyProfile</title>
+    </head>
+    
+    <body>
+        <h1>Myプロフィール編集画面</h1>
+    </body>
+    
+</html>
