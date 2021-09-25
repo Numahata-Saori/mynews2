7aaa904d979546a168f30d6826a414ac631377b0
NumahataSaori <saorin.kimura@gmail.com>
2021-09-25 08:53:38 +0000

    2021.09.25_php_laravel 12

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,16 @@ Route::get('/', function () {
 
 Route:: group(['prefix' => 'admin'], function() {
     
-    Route:: get('news/create', 'Admin\NewsController@add');
+    Route:: get('news/create', 'Admin\NewsController@add')->middleware('auth');
     
-    Route:: get('profile/create', 'Admin\ProfileController@add');
-    Route:: get('profile/edit', 'Admin\ProfileController@edit');
+    Route:: get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route:: get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
 });
 
 
 //Route:: get('XXX','AAAController@bbb');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
